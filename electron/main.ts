89cdf7643cc45651cc5f03526e8e8680027f773a
周俊杰 <zhoujunjie@MacBook-Air-3.local>
2025-12-31 14:00:31 +0800

    fix(v1.2.2): resolve window reopen issue

--- electron/main.ts
+++ electron/main.ts
@@ -30,6 +30,7 @@ if (!gotTheLock) {
         // Someone tried to run a second instance, focus our window
         if (win) {
             if (win.isMinimized()) win.restore()
+            win.show()
             win.focus()
         }
     })
@@ -421,6 +422,8 @@ app.on('before-quit', () => {
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
+    } else {
+        win?.show()
     }
 })
 
